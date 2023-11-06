62366261645dbd0feafc7e1321f1dc2d50871160
Kaito Tsutsui <ktsu2i@gmail.com>
2023-11-06 21:55:02 +0900

    Completed INFO STEP

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -89,10 +89,14 @@ const Modal: React.FC<ModalProps> = ({
                 >
                   <IoMdClose size={18} />
                 </button>
-                <div className="text-lg font-semibold">{title}</div>
+                <div className="text-lg font-semibold">
+                  {title}
+                </div>
               </div>
               {/* body */}
-              <div className="relative p-6 flex-auto">{body}</div>
+              <div className="relative p-6 flex-auto">
+                {body}
+              </div>
               {/* footer */}
               <div className="flex flex-col gap-2 p-6">
                 <div className="flex flex-row items-center gap-4 w-full">
