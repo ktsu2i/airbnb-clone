e244129046abf9a5c7a5c91fd45c0922a5289480
Kaito Tsutsui <ktsu2i@gmail.com>
2023-11-07 21:33:05 +0900

    Completed reservation component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,16 +4,16 @@ import { useCallback, useMemo } from "react"
 import { format } from "date-fns"
 import Image from "next/image"
 
-import { SafeUser } from "@/app/types"
+import { SafeListing, SafeUser } from "@/app/types"
 
-import { Listing, Reservation } from "@prisma/client"
+import { Reservation } from "@prisma/client"
 
 import useCountries from "@/app/hooks/useCountries"
 import HeartButton from "../HeartButton"
 import Button from "../Button"
 
 interface ListingCardProps {
-  data: Listing
+  data: SafeListing
   reservation?: Reservation
   onAction?: (id: string) => void
   disabled?: boolean
