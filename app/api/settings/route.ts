9d476fb316e6b613a53cf929e6264f4cb8c9fb7f
Kaito Tsutsui <ktsu2i@gmail.com>
2023-11-11 18:58:06 +0900

    Fixed

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -7,22 +7,26 @@ import getCurrentUser from "@/app/actions/getCurrentUser"
 export async function POST(
   request: Request
 ) {
-  const currentUser = await getCurrentUser()
-  const body = await request.json()
-  const { image } = body
+  try {
+    const currentUser = await getCurrentUser()
+    const body = await request.json()
+    const { image } = body
 
-  if (!currentUser?.id) {
-    return new Error("Something went wrong")
-  }
-
-  const updatedUser = await prisma.user.update({
-    where: {
-      id: currentUser.id
-    },
-    data: {
-      image: image
+    if (!currentUser?.id) {
+      return new Error("Something went wrong")
     }
-  })
 
-  return NextResponse.json(updatedUser)
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id
+      },
+      data: {
+        image: image
+      }
+    })
+
+    return NextResponse.json(updatedUser)
+  } catch (error: any) {
+    return NextResponse.error()
+  }
 }
