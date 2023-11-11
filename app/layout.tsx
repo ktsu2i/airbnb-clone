9babe32aad0a23dede644da5c20cfa6021b45ddf
Kaito Tsutsui <ktsu2i@gmail.com>
2023-11-11 14:43:49 +0900

    Completed filter functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ClientOnly from './components/ClientOnly'
 import RegisterModal from './components/modals/RegisterModal'
 import LoginModal from "./components/modals/LoginModal"
 import RentModal from "./components/modals/RentModal"
+import SearchModal from "./components/modals/SearchModal"
 
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
@@ -33,6 +34,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <RegisterModal />
           <LoginModal />
